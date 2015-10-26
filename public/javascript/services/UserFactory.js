00c4189b614bf12b56bab4139ce9a494a77e236d
ajahannes <aguerojahannes@gmail.com>
2015-10-26 16:09:38 -0700

    Added Login/Register features

--- public/javascript/services/UserFactory.js
+++ public/javascript/services/UserFactory.js
@@ -5,6 +5,24 @@
 
 	function UserFactory($http, $q) {
 		var o = {};
+		o.status ={};
+
+		o.loginUser
+
+		o.registerUser = function(){
+			var q = $q.defer();
+			$http.post("/api/user/register", user).then(function(res){
+				setToken(res.data); // Token is not yet parsed into JSON obj
+				setUser();
+				q.resolve(res.data);
+			});
+return q.promise;
+		}
+
+		o.logout = function(){
+
+		}
+
 
 		return o;
 	}
