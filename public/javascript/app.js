0472b193afa3bd6bba1a91f7fcc0996cdeecb837
soggust <soggust@gmail.com>
2015-10-26 18:08:45 -0500

    Index/Home Updated

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,6 +1,6 @@
 (function() {
 	'use strict';
-	angular.module('app', ['ui.router', "ngAnimate", "ngAria", "ngMaterial"])
+	angular.module('app', ['ui.router', "ngMaterial"])
 	.config(Config);
 	Config.$inject = ['$stateProvider', '$urlRouterProvider'];
 	function Config($stateProvider, $urlRouterProvider) {
