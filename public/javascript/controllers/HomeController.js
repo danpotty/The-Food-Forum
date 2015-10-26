769bb448efb479ee657fe5f73fdb1fc6a3f276b2
danpotty <danpotmail@yahoo.com>
2015-10-26 15:54:09 -0700

    switched add controllers

--- public/javascript/controllers/HomeController.js
+++ public/javascript/controllers/HomeController.js
@@ -5,26 +5,7 @@
 
 	function HomeController() {
 		var vm = this;
-		vm.topics = {
-			topicTitle: "test",
-			author: "test"
-		};
-		vm.comments = {
-			user: "test",
-			userComment: "test"
-		};
 
-		vm.createTopic = function(){
-			GlobalFactory.createTopic(vm.topic).then(function(){
-				$state.go('Subforum');
-			});
-		};
-
-		vm.createComment = function(){
-			GlobalFactory.createComment(vm.comment).then(function(){
-				$state.go('topic');
-			});
-		};
 
 
 	}
