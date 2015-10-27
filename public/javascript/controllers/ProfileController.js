72fd03ddfca95d47f58d678ff2337d147c207f9a
danpotty <danpotmail@yahoo.com>
2015-10-27 15:26:19 -0700

    setting up profile page edit

---
+++ public/javascript/controllers/ProfileController.js
@@ -0,0 +1,18 @@
+(function() {
+	'use strict';
+	angular.module('app')
+	.controller('ProfileController', ProfileController);
+
+	function ProfileController(UserFactory, $state) {
+		var vm = this;
+		vm.editting = false;
+		vm.selectedIndex = 0;
+
+    vm.editProfile = function(){
+      UserFactory.editProfile();
+    };
+
+
+
+	}
+})();
