be6993202de3162cc06c7f11dd31ef20838d3cc4
soggust <soggust@gmail.com>
2015-10-27 15:04:00 -0500

    Topics/Comments Working - Needs User Functionality To Be Integrated

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -37,41 +37,6 @@ router.post("/", function(req, res, next) {
    });
  });
 
-//
-// router.get('/', function(req, res, next){
-//   CommentPost.find({}).exec(function(err, result){
-//     if(err) return next(err);
-//     res.send(result);
-//   });
-// });
-//
-// router.get('/:id', function(req, res, next){
-//   CommentPost.findOne({_id: req.params.id}, function(err, result){
-//     if(err) return next(err);
-//     if(!result) return next('Could not find that comment.');
-//     res.send(result);
-//   });
-// });
-//
-// router.post('/:id/comment', function(req, res, next){
-//   var comment = {
-//     content: req.body.body,
-//     user: req.payload._id,
-//     date: Date,
-//     upvote: 0,
-//     spam: 0
-//   };
-//   CommentPost.findOne({_id: req.params.id}, function(err, topic){
-//     if(err) return next(err);
-//     if(!topic) return next('Could not find that topic');
-//     topic.comments.push(comment);
-//     topic.save(function(err, result){
-//       res.send(topic);
-//     });
-//   });
-// });
-//
-//
 
 
 module.exports = router;
