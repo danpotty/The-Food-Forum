9472c8e5a529f3cd284a4ad726285168e3d192fc
ajahannes <aguerojahannes@gmail.com>
2015-10-28 10:18:09 -0700

    modals, client feedbaack on login/register

--- public/javascript/controllers/GlobalController.js
+++ public/javascript/controllers/GlobalController.js
@@ -6,11 +6,15 @@
 	function GlobalController(UserFactory, $state) {
 		var nav = this;
 		nav.user = {};
+		nav.err = "";
 		nav.status = UserFactory.status;
 
 		nav.loginUser = function(){
-			UserFactory.loginUser(nav.user).then(function(){
+			UserFactory.loginUser(nav.user).then(function(res){
+				console.log(res);
 				$state.go("Home");
+			}, function(err){
+				nav.err = err;
 			});
 		};
 
